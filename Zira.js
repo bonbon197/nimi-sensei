382d8e8b14348f012290789d07e94765f8123c20
bonbon <nepunepubon@gmail.com>
2019-12-13 08:08:59 +0800

    added listener to port 5000

--- Zira.js
+++ Zira.js
@@ -9,6 +9,8 @@ const DB = require('./src/DB.js');
 const Utils = require('./src/Utils.js');
 const IPC = require('./src/IPC.js');
 
+var http = require('http'); http.createServer(function (req, res) { res.writeHead(200, {'Content-Type': 'text/plain'}); res.send('it is running\n'); }).listen(process.env.PORT || 5000);
+
 class Zira {
   constructor({
     firstShardID,
